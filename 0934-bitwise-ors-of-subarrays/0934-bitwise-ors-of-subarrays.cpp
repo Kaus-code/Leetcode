3f7adeb17cfda6877bd5d15a3b33177faf3bb5e2
K$TB <multitasker12678@gmail.com>
2025-07-31 14:19:32 +0530

    Time: 889 ms (79.62%), Space: 329.1 MB (65.75%) - LeetHub

---
+++ 0934-bitwise-ors-of-subarrays/0934-bitwise-ors-of-subarrays.cpp
@@ -0,0 +1,24 @@
+
+
+class Solution {
+public:
+    int subarrayBitwiseORs(vector<int>& A) {
+        unordered_set<int> ans;
+        set<int> cur;
+        cur.insert(0);
+
+        for (int x : A) {
+            set<int> cur2;
+            for (int y : cur) {
+                cur2.insert(x | y);
+            }
+            cur2.insert(x);
+            cur = cur2;
+            for (int val : cur) {
+                ans.insert(val);
+            }
+        }
+
+        return ans.size();
+    }
+};
