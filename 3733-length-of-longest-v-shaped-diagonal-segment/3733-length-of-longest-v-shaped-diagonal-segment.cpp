37de1640097cde02fe87207b9c4431a61204eff1
K$TB <multitasker12678@gmail.com>
2025-08-28 19:21:49 +0530

    Time: 264 ms (81.5%), Space: 104.1 MB (66%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.cpp
@@ -1,47 +1,52 @@
 class Solution {
 public:
+
+    vector<vector<int>> directions = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+    int m, n;
+    int t[501][501][4][2];
+
+    int solve(int i, int j, int d, bool canTurn, int val, vector<vector<int>>& grid) {
+        int i_ = i + directions[d][0];
+        int j_ = j + directions[d][1];
+
+        if(i_ < 0 || i_ >= m || j_ < 0 || j_ >= n || grid[i_][j_] != val) {
+            return 0;
+        }
+
+        if(t[i_][j_][d][canTurn] != -1) {
+            return t[i_][j_][d][canTurn];
+        }
+
+        int result = 0;
+        int keepMoving = 1 + solve(i_, j_, d, canTurn, val == 2 ? 0 : 2, grid);
+        result = max(result, keepMoving);
+
+        if(canTurn == true) {
+            int turnAndMove = max(keepMoving, 1 + solve(i_, j_, (d+1)%4, false, val == 2 ? 0 : 2, grid));
+            result = max(result, turnAndMove);
+        }
+
+        return t[i_][j_][d][canTurn] = result;
+    }
+
     int lenOfVDiagonal(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        int dirs[4][2] = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
-        int memo[m][n][4][2];
-        memset(memo, -1, sizeof(memo));
+        m = grid.size();
+        n = grid[0].size();
+        memset(t, -1, sizeof(t));
 
-        function<int(int, int, int, bool, int)> dfs =
-            [&](int cx, int cy, int direction, bool turn, int target) -> int {
-            int nx = cx + dirs[direction][0];
-            int ny = cy + dirs[direction][1];
-            /* If it goes beyond the boundary or the next node's value is not
-             * the target value, then return */
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n ||
-                grid[nx][ny] != target) {
-                return 0;
-            }
-            if (memo[nx][ny][direction][turn] != -1) {
-                return memo[nx][ny][direction][turn];
-            }
+        int result = 0;
 
-            /* Continue walking in the original direction. */
-            int maxStep = dfs(nx, ny, direction, turn, 2 - target);
-            if (turn) {
-                /* Clockwise rotate 90 degrees turn */
-                maxStep = fmax(maxStep, dfs(nx, ny, (direction + 1) % 4, false,
-                                            2 - target));
-            }
-            memo[nx][ny][direction][turn] = maxStep + 1;
-            return maxStep + 1;
-        };
-
-        int res = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (grid[i][j] == 1) {
-                    for (int direction = 0; direction < 4; ++direction) {
-                        res = fmax(res, dfs(i, j, direction, true, 2) + 1);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == 1) {
+                    for(int d = 0; d <= 3; d++) {
+                        result = max(result, 1 + solve(i, j, d, true, 2, grid));
                     }
                 }
             }
         }
 
-        return res;
+        return result;
+
     }
 };
