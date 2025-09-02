af752adfc0ba8ae4de94fa1b1b2de21e8b1a80f5
K$TB <multitasker12678@gmail.com>
2025-09-02 23:12:22 +0530

    Time: 0 ms (100%), Space: 32.5 MB (74.81%) - LeetHub

---
+++ 3278-find-the-number-of-ways-to-place-people-i/3278-find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size();
+        int result = 0;
+
+        // Sort by x ascending, and for same x, by y descending
+        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+            if (a[0] == b[0])
+                return a[1] > b[1]; // For same x, higher y comes first
+            return a[0] < b[0];     // Increasing x
+        });
+
+        for (int i = 0; i < n; ++i) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+
+            int maxY = INT_MIN;
+
+            for (int j = i + 1; j < n; ++j) {
+                int x2 = points[j][0];
+                int y2 = points[j][1];
+
+                if (y2 > y1) continue;
+
+                if (y2 > maxY) {
+                    ++result;
+                    maxY = y2;
+                }
+            }
+        }
+
+        return result;
+    }
+};
