a65022a752e33195d8a8a6ae48e53464a4f3f163
K$TB <multitasker12678@gmail.com>
2025-09-09 21:52:16 +0530

    Time: 15 ms (30.69%), Space: 9.3 MB (68.56%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int M=1e9+7;
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        vector<long long> dp(n+1);
+        dp[1]=1;
+        for(int day=2;day<=n;day++)
+        {
+            long long count=0;
+            for(int prev=day-forget+1;prev<=day-delay;prev++)
+            {
+                if(prev>0)
+                count=(count+dp[prev])%M;
+            }
+            dp[day]=count;
+        }
+
+        int result=0;
+        for(int day=n-forget+1;day<=n;day++)
+        {
+            if(day>0)
+            result=(result+dp[day])%M;
+        }
+
+        return result;
+    }
+};
