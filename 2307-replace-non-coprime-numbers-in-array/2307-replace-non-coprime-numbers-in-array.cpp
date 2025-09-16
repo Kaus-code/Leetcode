1c083124cef623b61624a581827670704407231c
K$TB <multitasker12678@gmail.com>
2025-09-16 20:52:39 +0530

    Time: 57 ms (9.72%), Space: 148.8 MB (5.09%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.cpp
@@ -1,45 +1,46 @@
 class Solution {
 public:
-int gcd(int a,int b)
-{
-    if(b==0)
-         return a;
-    else
-      return gcd(b,a%b);
-}
-
-bool ncm(int x,int y)
-{
-    if(gcd(x,y)>1)
-    {
-        return true;
+    int gcd(int a,int b) {
+        if(b==0) return a;
+        return gcd(b,a%b);
     }
-    return false;
-}
-long long lcm(int a,int b)
-{
-    return (a/gcd(a,b))*b;
-}
+
+    bool ncm(int x,int y) { // non-coprime?
+        return gcd(x,y) > 1;
+    }
+
+    long long lcm(int a,int b) {
+        return 1LL * a / gcd(a,b) * b;
+    }
+
     vector<int> replaceNonCoprimes(vector<int>& nums) {
-     vector<int> ans;
-     
-     for(int i=0;i<nums.size();i++)
-     {
-        ans.push_back(nums[i]);
-        while(ans.size()>1)
-        {
-            int prev_val=ans[ans.size()-2];
-            int next_val=ans[ans.size()-1];
-        if(!ncm(prev_val,next_val))
-        {
-break;
+        stack<long long> ans;
+        vector<int> res;
+
+        for (int i=0;i<nums.size();i++) {
+            long long cur = nums[i];
+            ans.push(cur);
+
+            while(ans.size()>=2) {
+                long long top1 = ans.top(); ans.pop();
+                long long top2 = ans.top(); ans.pop();
+
+                if (!ncm(top1, top2)) {
+                    ans.push(top2);
+                    ans.push(top1);
+                    break;
+                }
+
+                long long LCM = lcm(top1, top2);
+                ans.push(LCM);
+            }
         }
-        ans.pop_back();
-        ans.pop_back();
-        long long LCM=lcm(prev_val,next_val);
-        ans.push_back(int(LCM));
+
+        while(!ans.empty()) {
+            res.push_back((int)ans.top());
+            ans.pop();
         }
-     }
-     return ans;   
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
