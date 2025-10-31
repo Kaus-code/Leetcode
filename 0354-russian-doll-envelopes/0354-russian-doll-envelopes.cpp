36a7c67f5f2a4ebe65a53b1349955800452491e8
K$TB <multitasker12678@gmail.com>
2025-10-31 22:02:04 +0530

    Time: 35 ms (91.21%), Space: 84.2 MB (42.73%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxEnvelopes(vector<vector<int>>& envelopes) {
+
+        sort(envelopes.begin(), envelopes.end(), [](auto &a, auto &b) {
+            if (a[0] == b[0]) return a[1] > b[1];
+            return a[0] < b[0];
+        });
+
+        // Step 2: Extract heights only
+        vector<int> heights;
+        for (auto &env : envelopes) {
+            heights.push_back(env[1]);
+        }
+
+       
+        vector<int> lis;
+        for (int h : heights) {
+            auto it = lower_bound(lis.begin(), lis.end(), h);
+            if (it == lis.end()) lis.push_back(h);
+            else *it = h;  // replace to keep smallest tail
+        }
+
+        return lis.size();
+    }
+};
+
+          
+
