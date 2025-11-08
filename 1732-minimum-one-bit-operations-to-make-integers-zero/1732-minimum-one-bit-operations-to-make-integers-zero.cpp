8c34f3f75c918bf4cdd82bde0c21aefafda533c6
K$TB <multitasker12678@gmail.com>
2025-11-08 19:36:44 +0530

    Time: 0 ms (100%), Space: 8.1 MB (30.77%) - LeetHub

---
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int ans=n;
+        ans^=(ans>>1);
+        ans^=(ans>>2);
+        ans^=(ans>>4);
+        ans^=(ans>>8);
+        ans^=(ans>>16);
+
+        return ans;
+    }
+};
