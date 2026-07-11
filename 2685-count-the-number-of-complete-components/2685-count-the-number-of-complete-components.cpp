913ac95b48b66c7371352d161b53bfea12b64ff8
K$TB <multitasker12678@gmail.com>
2026-07-12 02:00:32 +0530

    Time: 18 ms (92.44%), Space: 118.9 MB (93.59%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,31 +1,73 @@
 class Solution {
 public:
+     typedef pair<int,int> p;
+     vector<int> parent;
+     vector<int> rank;
+
+     int find(int x)
+     {
+        if(x==parent[x]) return x;
+
+        return parent[x]=find(parent[x]);
+     }
+     void Union(int x, int y)
+     {
+        int x_parent=find(x);
+        int y_parent=find(y);
+
+        if(x_parent==y_parent) return;
+        if(rank[x_parent]>rank[y_parent])
+        {
+            parent[y_parent]=x_parent;
+        }
+        else if(rank[x_parent]<rank[y_parent])
+        {
+            parent[x_parent]=y_parent;
+        }
+       else
+        {
+         parent[x_parent]=y_parent;
+         rank[y_parent]++;
+        }
+     }
+
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> graph(n);
-        unordered_map<string, int> componentFreq;
-        for (int vertex = 0; vertex < n; vertex++) {
-            graph[vertex].push_back(vertex);
-        }
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
-        }
-        for (int vertex = 0; vertex < n; vertex++) {
-            vector<int> neighbors = graph[vertex];
-            sort(neighbors.begin(), neighbors.end());
-            string key;
-            for (int num : neighbors) {
-                key += to_string(num) + ",";
-            }
-            componentFreq[key]++;
-        }
-        int completeCount = 0;
-        for (const auto& entry : componentFreq) {
-            int size = count(entry.first.begin(), entry.first.end(), ',');
-            if (size == entry.second) {
-                completeCount++;
-            }
-        }
-        return completeCount;
+     int ans=0;
+     parent.resize(n);
+     rank.assign(n,0);
+     
+     for(int i=0;i<n;i++)
+     {
+        parent[i]=i;
+     }
+
+//Union all edges
+   for(auto &e:edges)
+   {
+Union(e[0],e[1]);
+   }
+    unordered_map<int,int> vertices;
+    //count vertices
+    for(int i=0;i<n;i++)
+    {
+vertices[find(i)]++;
+    }
+unordered_map<int,int>edgecount;
+//count edges
+for(auto&e:edges)
+{
+edgecount[find(e[0])]++;
+}
+
+for(auto &[root,v]:vertices)
+{
+    int expected=v*(v-1)/2;
+
+    if(edgecount[root]==expected)
+    {
+        ans++;
+    }
+}
+return ans;
     }
 };
